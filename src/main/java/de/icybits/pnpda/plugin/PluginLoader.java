26e9e1d5220e19ba2669b728c25afe626de00b14
Iceac Sarutobi <iceac88@gmail.com>
2019-06-27 20:56:04 +0200

    Complete rework of PluginLoader

--- src/main/java/de/icybits/pnpda/plugin/PluginLoader.java
+++ src/main/java/de/icybits/pnpda/plugin/PluginLoader.java
@@ -1,74 +1,125 @@
 package de.icybits.pnpda.plugin;
 
-import static java.util.Objects.requireNonNull;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.jar.JarFile;
 
 /**
- * Load plugin classes from jars in given folder.
+ * Load plugin classes from jars under given URI/Path.
+ * TODO add example to class description.
  *
  * @author Iceac Sarutobi
  */
 public class PluginLoader {
 
-  public static final String PLUGIN_CLASS = "Plugin-Class";
-
-  private final File folder;
+  private static final Logger log = LogManager.getLogger(PluginLoader.class);
+  private static final String PLUGIN_CLASS = "Plugin-Class";
 
   /**
-   * Initialize with the given folder.
+   * Load Plugins at given URI.
    *
-   * @param folder the folder to look in
-   * @throws NullPointerException If the given folder is null.
+   * @param source the uri to a jar or folder
+   * @return The {@link Result} containing the loaded Plugins and/or exceptions.
+   * @throws IOException Most likely if the given file or folder is not accessible.
    */
-  public PluginLoader(File folder) {
-    this.folder = requireNonNull(folder, "The folder parameter must not be null.");
-  }
-
-  public File getFolder() {
-    return this.folder;
+  public static Result load(URI source) throws IOException {
+    return load(Paths.get(source));
   }
 
   /**
-   * Load all {@link Plugin} classes from the jar files.
+   * Load Plugins at given Path.
    *
-   * @return Instances of all found plugins.
+   * @param source the uri to a jar or folder
+   * @return The {@link Result} containing the loaded Plugins and/or exceptions.
+   * @throws IOException Most likely if the given file or folder is not accessible.
    */
-  public List<Plugin> load() {
-    File[] files = this.getFolder().listFiles(this::isJar);
-    if (files == null) {
-      return new ArrayList<>();
-    }
-    List<Plugin> pluginList = new ArrayList<>();
-    for (File file : files) {
+  public static Result load(Path source) throws IOException {
+    final List<Plugin> pluginList = new ArrayList<>();
+    final List<File> fileList = new ArrayList<>();
+    final List<Exception> exceptionList = new ArrayList<>();
+
+    Files.walkFileTree(source, new JarCollector(fileList));
+
+    for (File file : fileList) {
       try {
-        // TODO Optimize code.
-        JarFile jarFile = new JarFile(file);
-        String className = jarFile.getManifest().getMainAttributes().getValue(PLUGIN_CLASS);
-        URLClassLoader loader = new URLClassLoader(new URL[]{file.toURI().toURL()});
-        Class<?> pluginClass = loader.loadClass(className);
-        Class<? extends Plugin> plugin = pluginClass.asSubclass(Plugin.class);
-        Plugin instance = plugin.newInstance();
-        pluginList.add(instance);
-      } catch (IOException | ClassNotFoundException | ClassCastException | IllegalAccessException | InstantiationException e) {
-        e.printStackTrace();
-        // TODO Logging when available.
+        pluginList.add(getPluginFromJarFile(file));
+      } catch (IOException | ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+        log.debug(e);
+        exceptionList.add(e);
       }
     }
-    return pluginList;
+
+    return new Result() {
+      @Override
+      public List<Plugin> getPlugins() {
+        return Collections.unmodifiableList(pluginList);
+      }
+
+      @Override
+      public List<Exception> getExceptions() {
+        return Collections.unmodifiableList(exceptionList);
+      }
+    };
   }
 
-  private boolean isJar(File file) {
-    if (file != null) {
-      return file.getName().endsWith(".jar");
+  private static Plugin getPluginFromJarFile(File file) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+    JarFile jarFile = new JarFile(file);
+    String className = jarFile.getManifest().getMainAttributes().getValue(PLUGIN_CLASS);
+    URLClassLoader loader = new URLClassLoader(new URL[]{file.toURI().toURL()});
+    Class<?> pluginClass = loader.loadClass(className);
+    Class<? extends Plugin> plugin = pluginClass.asSubclass(Plugin.class);
+    return plugin.newInstance();
+  }
+
+  /**
+   * The Result of the PluginLoader load method. Contains the Plugins and/or Exceptions.
+   */
+  public interface Result {
+
+    List<Plugin> getPlugins();
+
+    List<Exception> getExceptions();
+
+  }
+
+  private static class JarCollector extends SimpleFileVisitor<Path> {
+
+    private final List<File> destination;
+
+    JarCollector(List<File> destination) {
+      this.destination = destination;
     }
-    return false;
-  }
 
+    @Override
+    public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
+      if (attrs.isDirectory() || attrs.isOther() || attrs.isSymbolicLink() || !attrs.isRegularFile()) {
+        log.debug("non file in visitFile method: " + path);
+        return FileVisitResult.CONTINUE;
+      }
+      File file = path.toFile();
+      if (!isJar(file)) {
+        log.debug("non jar file in visitFile method: " + path);
+        return FileVisitResult.CONTINUE;
+      }
+      log.debug("jar file found: " + path);
+      this.destination.add(file);
+      return FileVisitResult.CONTINUE;
+    }
+
+    private boolean isJar(File file) {
+      String name = file.getName();
+      return name.endsWith(".jar") || name.endsWith(".zip");
+    }
+  }
 }
